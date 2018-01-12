b73db6cf100ffa905b69c29129e7a5ac2f275ebb
White Fossa <fossa@main.fossa.fossa>
2018-01-12 23:01:48 +0300

    Removed all junk from project, added clock setup

---
+++ src/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ *  Created on: 12 янв. 2018 г.
+ *      Author: fossa
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "HAL/hal.h"
+
+/**
+ * Entry point.
+ */
+int main(int argc, char* argv[]);
+
+
+#endif /* MAIN_H_ */
